7ff1ffe4b7bfe1faaf303ba809ce43b912f4215e
zdat5624 <zdat5624@gmail.com>
2024-10-09 12:07:38 +0700

    them form quan ly dich vu cu dan

---
+++ DesignEasyHouse1/formsPhongBan/formQuanLyYeuCauTuCuDan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesignEasyHouse1.formsPhongBan
+{
+    public partial class formQuanLyYeuCauTuCuDan : Form
+    {
+        public formQuanLyYeuCauTuCuDan()
+        {
+            InitializeComponent();
+        }
+    }
+}
